7b2a1ffe9c7cddaa92ac92fa994ace956a2362bf
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-08-20 19:30:42 +0300

    fixed gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp") version "1.9.0-1.0.12"
 }
 
 android {
@@ -46,10 +47,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.compiler)
     implementation(libs.androidx.live.data)
     implementation(libs.androidx.view.model)
     implementation(libs.androidx.preference.ktx)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
