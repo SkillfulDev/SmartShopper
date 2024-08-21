11ee49799259682b01d4ab401ab7d1ef754e7529
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-08-21 14:45:31 +0300

    added shopping list entity

---
+++ app/src/main/java/ua/chernonog/smartshopper/entity/ShoppingList.kt
@@ -0,0 +1,22 @@
+package ua.chernonog.smartshopper.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "shopping_list")
+data class ShoppingList(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @ColumnInfo("name")
+    val name: String,
+    @ColumnInfo(name = "time")
+    val time: String,
+    @ColumnInfo(name = "totalItems")
+    val totalItems: Int,
+    @ColumnInfo(name = "boughtItems")
+    val boughtItems: Int,
+    @ColumnInfo(name = "itemsIds")
+    val itemsIds: String
+) : Serializable
