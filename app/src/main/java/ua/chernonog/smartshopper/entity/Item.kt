782016d0ce7b3970a7c34eda82854be1c16c27ff
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-08-21 15:18:50 +0300

    added item,library,notebook entity

---
+++ app/src/main/java/ua/chernonog/smartshopper/entity/Item.kt
@@ -0,0 +1,21 @@
+package ua.chernonog.smartshopper.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "itemInfo")
+    val itemInfo: String?,
+    @ColumnInfo(name = "isBought")
+    val isBought: Boolean = false,
+    @ColumnInfo(name = "shoppingListId")
+    val shoppingListId: Int,
+    @ColumnInfo(name = "itemType")
+    val itemType: String = "item"
+)
