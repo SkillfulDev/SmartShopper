13ec5b104a5d44043be93e7e761da8df90f4beff
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-08-22 16:17:05 +0300

    added bottom navigation

--- app/src/main/java/ua/chernonog/smartshopper/activity/MainActivity.kt
+++ app/src/main/java/ua/chernonog/smartshopper/activity/MainActivity.kt
@@ -1,12 +1,30 @@
 package ua.chernonog.smartshopper.activity
 
 import android.os.Bundle
+import android.util.Log
+
 import androidx.appcompat.app.AppCompatActivity
 import ua.chernonog.smartshopper.R
+import ua.chernonog.smartshopper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onBottomNavigationListener()
+    }
+
+    private fun onBottomNavigationListener() {
+        binding.bnvMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.settings -> Log.d("MyLog", "Settings")
+                R.id.notes -> Log.d("MyLog", "Notes")
+                R.id.shopList -> Log.d("MyLog", "Shop List")
+                R.id.item -> Log.d("MyLog", "Item")
+            }
+            true
+        }
     }
 }
