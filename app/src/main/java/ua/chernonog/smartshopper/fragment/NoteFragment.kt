ff831c76e87d2c61a034bc9be78e8a0129510e7f
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-08-23 17:19:54 +0300

    added list adapter

--- app/src/main/java/ua/chernonog/smartshopper/fragment/NoteFragment.kt
+++ app/src/main/java/ua/chernonog/smartshopper/fragment/NoteFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import ua.chernonog.smartshopper.databinding.FragmentNoteBinding
 
 class NoteFragment : BaseFragment() {
@@ -23,6 +22,6 @@ class NoteFragment : BaseFragment() {
     }
 
     override fun onClickNew() {
-       TODO()
+        TODO()
     }
 }
